4cb3360fc70b2088391a8c36adb0e782b29f463d
Mapachana <anabuenrua99@gmail.com>
2020-10-19 11:13:07 +0200

    Añadida practica 1

---
+++ plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/DependInfo.cmake
@@ -0,0 +1,43 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/animacion.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/animacion.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/array-verts.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/array-verts.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/camara.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/camara.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/escena.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/escena.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/grafo-escena.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/grafo-escena.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/main.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/main.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/malla-ind.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/malla-ind.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/malla-revol.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/malla-revol.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/materiales-luces.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/materiales-luces.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/objeto3d.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/objeto3d.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/seleccion.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/seleccion.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/cauce.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/cauce.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/fbo.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/fbo.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/ig-aux.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/ig-aux.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/lector-jpg.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/lector-jpg.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/lector-ply.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/lector-ply.cpp.o"
+  "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/matrices-tr.cpp" "/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/matrices-tr.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "LINUX"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "../../../plantilla/include"
+  "../../../recursos/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ plantilla/cmake-unix/build/CMakeFiles/pracs_ig_exe.dir/cmake_clean.cmake
@@ -0,0 +1,26 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/malla-revol.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/camara.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/malla-ind.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/main.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/materiales-luces.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/objeto3d.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/grafo-escena.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/animacion.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/seleccion.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/escena.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/plantilla/src/array-verts.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/ig-aux.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/matrices-tr.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/cauce.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/lector-ply.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/fbo.cpp.o"
+  "CMakeFiles/pracs_ig_exe.dir/home/ana/Escritorio/Universidad/4_CUARTO/1_Cuatri/IG/practicas/materiales-ig/recursos/src/lector-jpg.cpp.o"
+  "pracs_ig_exe.pdb"
+  "pracs_ig_exe"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/pracs_ig_exe.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
